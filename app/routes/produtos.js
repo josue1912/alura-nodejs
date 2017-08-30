e76294da3c9809471e084d2aa05975db69a4758d
Josue Freitas <josue1912@gmail.com>
2017-08-30 08:11:07 -0300

    exibindo dados do banco de dados

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,17 @@
 module.exports = function(app){
   app.get("/produtos", function(req, res){
-      res.render("produtos/lista");
+    var mysql = require("mysql");
+    var connection = mysql.createConnection({
+      host : "localhost",
+      user : "root",
+      password : "",
+      database : "casadocodigo"
+    });
+
+    connection.query("select * from livros", function(err, result){
+      res.render("produtos/lista", {lista:result});
+    });
+
+    connection.end();
   });
 };
