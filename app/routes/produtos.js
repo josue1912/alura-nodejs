e8c0fbd898e34d28d04abd6b191e7c509ccbf9c3
Josue Freitas <josue1912@gmail.com>
2017-08-30 08:22:15 -0300

    isolando criacao de conexao com o bd

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,17 +1,12 @@
+var connectionFactory = require("../infra/connectionFactory")
+
 module.exports = function(app){
   app.get("/produtos", function(req, res){
-    var mysql = require("mysql");
-    var connection = mysql.createConnection({
-      host : "localhost",
-      user : "root",
-      password : "",
-      database : "casadocodigo"
-    });
+    var connection = connectionFactory();
 
     connection.query("select * from livros", function(err, result){
       res.render("produtos/lista", {lista:result});
     });
-
     connection.end();
   });
 };
